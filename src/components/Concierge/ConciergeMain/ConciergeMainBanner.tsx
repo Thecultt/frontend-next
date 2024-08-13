c6681772d03c93a1b8179ae0c164414e800b3a3c
nagibinstudio <nagibin.ars@gmail.com>
2024-08-13 12:40:35 +0300

    new/feature/1677: Updated Text

--- src/components/Concierge/ConciergeMain/ConciergeMainBanner.tsx
+++ src/components/Concierge/ConciergeMain/ConciergeMainBanner.tsx
@@ -7,43 +7,44 @@ import { getClassNames } from '@/functions/getClassNames';
 import ConciergeMainImage from '@/assets/images/concierge/concierge-main.jpg';
 
 interface Props {
-	scrollToForm: () => void;
+    scrollToForm: () => void;
 }
 
 const ConciergeMainBanner: React.FC<Props> = ({ scrollToForm }) => {
-	const isMobile = useMediaQuery(`(max-width: ${MEDIA_SIZES.tablet})`);
+    const isMobile = useMediaQuery(`(max-width: ${MEDIA_SIZES.tablet})`);
 
-	return (
-		<div
-			className="concierge-main"
-			style={{
-				backgroundImage: `url("${isMobile ? ConciergeMainImage.src : ''}")`,
-			}}
-		>
-			<div className="concierge-main-text">
-				<h2 className="title mb concierge-main-text__title">Вы выберете, мы доставим</h2>
-				<p className="description concierge-main-text__description">
-					Консьерж-сервис THE CULTT доставит для вас из&nbsp;Европы и&nbsp;США любые позиции с&nbsp;официальных сайтов Herm&egrave;s, Chanel, Cartier, Panerai и&nbsp;других культовых брендов.
-				</p>
-				<button
-					className={getClassNames('concierge-main-text__btn', {
-						btn: !isMobile,
-						'btn-light': isMobile,
-					})}
-					onClick={scrollToForm}
-				>
-					Заказать сейчас
-				</button >
-			</div >
+    return (
+        <div
+            className="concierge-main"
+            style={{
+                backgroundImage: `url("${isMobile ? ConciergeMainImage.src : ''}")`,
+            }}
+        >
+            <div className="concierge-main-text">
+                <h2 className="title mb concierge-main-text__title">Вы выберете, мы доставим</h2>
+                <p className="description concierge-main-text__description">
+                    Консьерж-сервис THE CULTT доставит для вас из&nbsp;Европы и&nbsp;США любые позиции
+                    с&nbsp;официальных сайтов Herm&egrave;s, Chanel, Cartier, Panerai и&nbsp;других культовых брендов.
+                </p>
+                <button
+                    className={getClassNames('concierge-main-text__btn', {
+                        btn: !isMobile,
+                        'btn-light': isMobile,
+                    })}
+                    onClick={scrollToForm}
+                >
+                    Заказать сейчас
+                </button>
+            </div>
 
-			<div
-				className="concierge-main-image"
-				style={{
-					backgroundImage: `url("${!isMobile ? ConciergeMainImage.src : ''}")`,
-				}}
-			></div >
-		</div >
-	);
+            <div
+                className="concierge-main-image"
+                style={{
+                    backgroundImage: `url("${!isMobile ? ConciergeMainImage.src : ''}")`,
+                }}
+            ></div>
+        </div>
+    );
 };
 
 export default ConciergeMainBanner;
--- src/screens/Concierge/ConciergeProduct.tsx
+++ src/screens/Concierge/ConciergeProduct.tsx
@@ -6,81 +6,84 @@ import { useParams } from 'next/navigation';
 
 import { useTypedSelector } from '@/hooks/useTypedSelector';
 import {
-	fetchConciergeProduct,
-	sendConciergeProductApplication,
-	setConciergeProductIsSendFormProductPage,
+    fetchConciergeProduct,
+    sendConciergeProductApplication,
+    setConciergeProductIsSendFormProductPage,
 } from '@/redux/actions/concierge';
 import { ConciergeProductInfo, ConciergeProductForm, PageLoader, Popup } from '@/components';
 
 const ConciergeProduct: React.FC = () => {
-	const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-	const { id } = useParams();
+    const { id } = useParams();
 
-	const { isLoadedProduct, product, isSendFormProductPageSuccess } = useTypedSelector(({ concierge }) => concierge);
+    const { isLoadedProduct, product, isSendFormProductPageSuccess } = useTypedSelector(({ concierge }) => concierge);
 
-	React.useEffect(() => {
-		dispatch(fetchConciergeProduct(id as string) as any);
-	}, []);
+    React.useEffect(() => {
+        dispatch(fetchConciergeProduct(id as string) as any);
+    }, []);
 
-	const onSubmitProductApplication = (data: any) => {
-		dispatch(sendConciergeProductApplication(data, parseInt(id as string)) as any);
-	};
+    const onSubmitProductApplication = (data: any) => {
+        dispatch(sendConciergeProductApplication(data, parseInt(id as string)) as any);
+    };
 
-	return (
-		<section className="concierge-product">
-			<Popup
-				state={isSendFormProductPageSuccess}
-				setState={() => dispatch(setConciergeProductIsSendFormProductPage(false))}
-			>
-				<div className="concierge-product-success">
-					<h4 className="concierge-product-success__title">Спасибо! Ваша заявка принята</h4>
-					<p className="concierge-product-success__subtitle">
-						Скоро мы свяжемся с вами в WhatsApp <br /> по указанному номеру телефона.
-					</p>
-				</div>
-			</Popup>
+    return (
+        <section className="concierge-product">
+            <Popup
+                state={isSendFormProductPageSuccess}
+                setState={() => dispatch(setConciergeProductIsSendFormProductPage(false))}
+            >
+                <div className="concierge-product-success">
+                    <h4 className="concierge-product-success__title">Спасибо! Ваша заявка принята</h4>
+                    <p className="concierge-product-success__subtitle">
+                        Скоро мы свяжемся с вами в WhatsApp <br /> по указанному номеру телефона.
+                    </p>
+                </div>
+            </Popup>
 
-			<div className="container">
-				<div className="concierge-product-wrapper">
-					<button className="concierge-product__back" onClick={() => window.history.back()}>
-						<svg width="8" height="15" viewBox="0 0 8 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-							<path
-								d="M7 1.5L1 7.5L7 13.5"
-								stroke="#202020"
-								strokeWidth="2"
-								strokeLinecap="round"
-								strokeLinejoin="round"
-							/>
-						</svg>
-						Вернуться
-					</button>
+            <div className="container">
+                <div className="concierge-product-wrapper">
+                    <button className="concierge-product__back" onClick={() => window.history.back()}>
+                        <svg width="8" height="15" viewBox="0 0 8 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M7 1.5L1 7.5L7 13.5"
+                                stroke="#202020"
+                                strokeWidth="2"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                            />
+                        </svg>
+                        Вернуться
+                    </button>
 
-					{isLoadedProduct ? (
-						<>
-							<ConciergeProductInfo {...product} />
+                    {isLoadedProduct ? (
+                        <>
+                            <ConciergeProductInfo {...product} />
 
-							<ConciergeProductForm onSubmit={onSubmitProductApplication} />
+                            <ConciergeProductForm onSubmit={onSubmitProductApplication} />
 
-							<div className="concierge-product-buyer-media">
-								<h3 className="concierge-product-buyer-media__title">Доставляется через консьерж-сервис</h3>
+                            <div className="concierge-product-buyer-media">
+                                <h3 className="concierge-product-buyer-media__title">
+                                    Доставляется через консьерж-сервис
+                                </h3>
 
-								<p className="concierge-product-buyer-media__subtitle">
-									Этот товар будет доставлен консьержем THE CULTT с официального сайта бренда. Подайте заявку, чтобы определить актуальную стоимость с учетом доставки.
-								</p>
+                                <p className="concierge-product-buyer-media__subtitle">
+                                    Этот товар будет доставлен консьержем THE CULTT с официального сайта бренда. Подайте
+                                    заявку, чтобы определить актуальную стоимость с учетом доставки.
+                                </p>
 
-								<p className="concierge-product-buyer-media__notoffer">
-									*предложение не является публичной офертой.
-								</p>
-							</div>
-						</>
-					) : (
-						<PageLoader />
-					)}
-				</div>
-			</div>
-		</section>
-	);
+                                <p className="concierge-product-buyer-media__notoffer">
+                                    *предложение не является публичной офертой.
+                                </p>
+                            </div>
+                        </>
+                    ) : (
+                        <PageLoader />
+                    )}
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default ConciergeProduct;
