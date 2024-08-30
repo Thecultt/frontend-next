1956562c500ccd1b113a4052a92f41a1a047c100
nagibin88 <nagibin.ars@gmail.com>
2024-08-30 14:26:50 +0300

    hotfix order validate email

--- src/components/Order/OrderForm/OrderForm.tsx
+++ src/components/Order/OrderForm/OrderForm.tsx
@@ -96,6 +96,8 @@ const OrderForm: React.FC<{} & InjectedFormProps<{}, {}>> = ({
         }
     }, [isLoggedIn, isLoaded]);
 
+    console.log(invalid);
+
     return (
         <form className="order-form" onSubmit={handleSubmit}>
             <OrderFormContact emailValue={emailValue} />
