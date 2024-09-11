1fb820b6b446b56af4b719ce3bb534845c327a20
Ilya <lekomtzev.ilja@gmail.com>
2024-09-11 14:53:38 +0300

    feature/1807: todo new input ui component

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,3 +1,4 @@
 export { ReactComponent as Logo, default as logoPath } from './logo.svg';
 export { ReactComponent as XIcon, default as xIconPath } from './x.svg';
 export { ReactComponent as CheckmarkIcon, default as checkmarkIconPath } from './checkmark.svg';
+export { ReactComponent as InfoIcon, default as infoIconPath } from './info.svg';
--- src/components/index.ts
+++ src/components/index.ts
@@ -29,6 +29,7 @@ export { BaseImage } from './UI/BaseImage/BaseImage';
 export { Badge } from './UI/Badge/Badge';
 export { Skeleton } from './UI/Skeleton/Skeleton';
 export { Button } from './UI/Button/Button';
+export { Input as NewInput } from './UI/Input/Input';
 
 // Notifications
 export { default as NotificationsServer } from './UI/NotificationsServer/NotificationsServer';
