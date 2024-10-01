19e21504da83fd1f925bf8a074c4e6669a7e6343
Ilya <lekomtzev.ilja@gmail.com>
2024-10-01 13:12:34 +0300

    catalog fixes

--- src/screens/Catalog/ServerCatalog.tsx
+++ src/screens/Catalog/ServerCatalog.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import type { ItemList as SchemaItemList, Product as SchemaProduct, WithContext } from 'schema-dts';
 
+import { FullscreenLoader } from '@/shared/ui';
 import CatalogProductsNull from '@/components/Catalog/CatalogProducts/CatalogProductsNull';
 import { GetCatalogResponse } from '@/types/api';
 import { APP_ROUTE } from '@/constants/routes';
@@ -52,55 +53,61 @@ const ServerCatalog: React.FC<Props> = ({ serverCatalogData, mainTitle }) => {
     };
 
     return (
-        <section className="static-catalog">
-            <h1 className="static-catalog-main-title">{mainTitle || 'Каталог'}</h1>
-            <nav className="static-catalog-sidebar">
-                <a
-                    href={`${APP_PROD_DOMAIN}${APP_ROUTE.catalog}`}
-                    className="static-catalog-sidebar__item"
-                    title="Каталог"
-                >
-                    Каталог
-                </a>
-                {Object.values(CATEGORY_SLUGS).map((slug) => (
+        <>
+            <FullscreenLoader />
+            <section className="static-catalog">
+                <h1 className="static-catalog-main-title">{mainTitle || 'Каталог'}</h1>
+                <nav className="static-catalog-sidebar">
                     <a
-                        key={slug}
-                        href={`${APP_PROD_DOMAIN}${APP_ROUTE.catalog}/${slug}`}
+                        href={`${APP_PROD_DOMAIN}${APP_ROUTE.catalog}`}
                         className="static-catalog-sidebar__item"
-                        title={CATEGORY_SLUG_NAMES[slug]}
+                        title="Каталог"
                     >
-                        {CATEGORY_SLUG_NAMES[slug]}
+                        Каталог
                     </a>
-                ))}
-            </nav>
-            <div className="static-catalog-products">
-                {items.map((item) => (
-                    <article key={item.article} className="static-catalog-products-item">
-                        <a href={`${APP_PROD_DOMAIN}${APP_ROUTE.product}/${item.article}`} title={item.name}>
-                            <img
-                                width="100%"
-                                className="static-catalog-products-item__image"
-                                src={item.images[0]}
-                                alt={item.name}
-                            />
+                    {Object.values(CATEGORY_SLUGS).map((slug) => (
+                        <a
+                            key={slug}
+                            href={`${APP_PROD_DOMAIN}${APP_ROUTE.catalog}/${slug}`}
+                            className="static-catalog-sidebar__item"
+                            title={CATEGORY_SLUG_NAMES[slug]}
+                        >
+                            {CATEGORY_SLUG_NAMES[slug]}
                         </a>
-                        <h2 className="static-catalog-products-item__title">
+                    ))}
+                </nav>
+                <div className="static-catalog-products">
+                    {items.map((item) => (
+                        <article key={item.article} className="static-catalog-products-item">
                             <a href={`${APP_PROD_DOMAIN}${APP_ROUTE.product}/${item.article}`} title={item.name}>
-                                {item.name}
+                                <img
+                                    width="100%"
+                                    className="static-catalog-products-item__image"
+                                    src={item.images[0]}
+                                    alt={item.name}
+                                />
                             </a>
-                        </h2>
-                        <p className="static-catalog-products-item__prices">
-                            <span className="static-catalog-products-item__price">{item.price}</span>
-                            {item.old_price && (
-                                <span className="static-catalog-products-item__old-price">{item.old_price}</span>
-                            )}
-                        </p>
-                    </article>
-                ))}
-            </div>
+                            <h2 className="static-catalog-products-item__title">
+                                <a href={`${APP_PROD_DOMAIN}${APP_ROUTE.product}/${item.article}`} title={item.name}>
+                                    {item.name}
+                                </a>
+                            </h2>
+                            <p className="static-catalog-products-item__prices">
+                                <span className="static-catalog-products-item__price">{item.price}</span>
+                                {item.old_price && (
+                                    <span className="static-catalog-products-item__old-price">{item.old_price}</span>
+                                )}
+                            </p>
+                        </article>
+                    ))}
+                </div>
 
-            <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(productsJsonLd) }} />
-        </section>
+                <script
+                    type="application/ld+json"
+                    dangerouslySetInnerHTML={{ __html: JSON.stringify(productsJsonLd) }}
+                />
+            </section>
+        </>
     );
 };
 
