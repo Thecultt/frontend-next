4175dd0ad5e6b1ec52fcf509ae3c8d5981eeed4d
nagibin88 <nagibin.ars@gmail.com>
2024-10-17 13:50:48 +0300

    feature/rtk-cabinet: Validation

--- src/components/Cabinet/CabinetSetting/CabinetSettingForm/CabinetSettingFormAddress/validate.ts
+++ src/components/Cabinet/CabinetSetting/CabinetSettingForm/CabinetSettingFormAddress/validate.ts
@@ -1,12 +1,12 @@
 import * as Yup from 'yup';
 
-import { YUP_REQUIRED_MIN_MAX_STRING } from '@/constants/validation';
+import { YUP_REQUIRED_MIN_MAX_STRING, YUP_NOT_REQUIRED_MIN_MAX_STRING } from '@/constants/validation';
 
 export const SCHEMA = Yup.object({
     country: YUP_REQUIRED_MIN_MAX_STRING,
     city: YUP_REQUIRED_MIN_MAX_STRING,
-    street: YUP_REQUIRED_MIN_MAX_STRING,
-    house: YUP_REQUIRED_MIN_MAX_STRING,
-    flat: YUP_REQUIRED_MIN_MAX_STRING,
-    comment: YUP_REQUIRED_MIN_MAX_STRING,
+    street: YUP_NOT_REQUIRED_MIN_MAX_STRING,
+    house: YUP_NOT_REQUIRED_MIN_MAX_STRING,
+    flat: YUP_NOT_REQUIRED_MIN_MAX_STRING,
+    comment: YUP_NOT_REQUIRED_MIN_MAX_STRING,
 });
--- src/components/Cabinet/CabinetSetting/CabinetSettingForm/CabinetSettingFormContact/validate.ts
+++ src/components/Cabinet/CabinetSetting/CabinetSettingForm/CabinetSettingFormContact/validate.ts
@@ -1,9 +1,9 @@
 import * as Yup from 'yup';
 
-import { YUP_REQUIRED_MIN_MAX_EMAIL, YUP_REQUIRED_MIN_MAX_STRING } from '@/constants/validation';
+import { YUP_REQUIRED_MIN_MAX_EMAIL, YUP_NOT_REQUIRED_MIN_MAX_STRING } from '@/constants/validation';
 
 export const SCHEMA = Yup.object({
     email: YUP_REQUIRED_MIN_MAX_EMAIL,
-    phone: YUP_REQUIRED_MIN_MAX_STRING,
-    username_telegram: YUP_REQUIRED_MIN_MAX_STRING,
+    phone: YUP_NOT_REQUIRED_MIN_MAX_STRING,
+    username_telegram: YUP_NOT_REQUIRED_MIN_MAX_STRING,
 });
